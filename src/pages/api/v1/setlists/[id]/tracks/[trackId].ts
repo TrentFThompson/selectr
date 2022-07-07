1d5b1453564e8828f62376dbbe76f33bb6dcf8c8
Trent Thompson <tthompson@conestogac.on.ca>
2022-07-07 18:35:46 -0400

    add deleting a track from a setlist

---
+++ src/pages/api/v1/setlists/[id]/tracks/[trackId].ts
@@ -0,0 +1,56 @@
+//
+//  File:         [trackId].ts
+//  Description:  Exports the handler for the /setlists/[id]/tracks/[trackId] api route
+//
+
+// Installed imports
+import { NextApiRequest, NextApiResponse } from "next";
+
+// Custom imports
+import { BadRequestError } from "@/utils/http/errors";
+import handleError from "@/utils/server/errors/handleError";
+import db from "@/utils/database";
+import { Collections } from "@/utils/database/collections";
+
+//
+//  Function:     handler
+//  Description:  handles facilitating the different request methods
+//  Params:       req: NextApiRequest - the request object
+//                res: NextApiResponse - the response object
+//
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    switch (req.method) {
+      case "DELETE": {
+        return await _delete(req, res);
+      }
+      default: {
+        throw new BadRequestError("Method not implemented");
+      }
+    }
+  } catch (error: any) {
+    return handleError(error, res);
+  }
+}
+
+//
+//  Function:     _delete
+//  Description:  remove a song from a specified setlist
+//  Params:       req: NextApiRequest - the request object
+//                res: NextApiResponse - the response object
+//  Returns:      N/A
+//
+async function _delete(req: NextApiRequest, res: NextApiResponse) {
+  const { id, trackId } = req.query;
+  return res
+    .status(204)
+    .json(
+      await db.remove(
+        `${Collections.Setlists}/${id}/${Collections.Tracks}`,
+        trackId
+      )
+    );
+}
