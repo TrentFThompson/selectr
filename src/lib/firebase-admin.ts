73558d0392f3df535d69e5c3513faad16c5feeb6
Trent Thompson <tthompson@conestogac.on.ca>
2022-07-07 08:49:11 -0400

    - add firebase - add firebase admin

---
+++ src/lib/firebase-admin.ts
@@ -0,0 +1,27 @@
+//
+//  File:         firebase-admin.ts
+//  Description:  This file exports the initialized firebase admin module.
+//
+
+// Installed imports
+import firebaseAdmin from "firebase-admin";
+
+// Initialize the firebase admin module
+if (!firebaseAdmin.apps.length) {
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert({
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\n/g, "\n"),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    }),
+    // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  });
+}
+
+// Setup exports
+// const auth = firebaseAdmin.auth();
+const firestore = firebaseAdmin.firestore();
+// const storage = firebaseAdmin.storage();
+
+export { firestore };
+export default firebaseAdmin;
---
+++ src/lib/firebase.ts
@@ -0,0 +1,24 @@
+//
+//  File:         firebase.ts
+//  Description:  This file exports the initialized firebase module.
+//
+
+// Installed imports
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  });
+}
+
+// setup auth
+// const auth = firebase.auth();
+// export { auth };
+
+export default firebase;
