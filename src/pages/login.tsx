11c0e35a9e800b6c39cd3088bf1d41374ce757a1
Trent Thompson <tthompson@conestogac.on.ca>
2022-07-20 10:22:28 -0400

    add redirect to index when authed on signup and login pages

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,11 +4,12 @@
 //
 
 // Installed imports
-import type { NextPage } from "next";
+import type { GetServerSidePropsContext, NextPage } from "next";
 import { Center } from "@chakra-ui/react";
 
 // Custom imports
 import LoginForm from "@/components/Auth/LoginForm";
+import authenticate from "@/utils/getServerSideProps/authenticate";
 
 //
 //  Component:    Login
@@ -21,5 +22,29 @@ const Login: NextPage = () => {
     </Center>
   );
 };
+
+//
+// Function:    getServerSideProps
+// Description: Fetches the initial data for this page
+// Parameters:  context: GetServerSidePropsContext - the context of the page
+// Returns:     default props
+//
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // If authentication works then we want to go to the index page
+  // Otherwise continue to login
+  try {
+    await authenticate(context);
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+    };
+  } catch {
+    return {
+      props: {},
+    };
+  }
+}
 
 export default Login;
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -4,11 +4,12 @@
 //
 
 // Installed imports
-import type { NextPage } from "next";
+import type { GetServerSidePropsContext, NextPage } from "next";
 import { Center } from "@chakra-ui/react";
 
 // Custom imports
 import SignupForm from "@/components/Auth/SignupForm";
+import authenticate from "@/utils/getServerSideProps/authenticate";
 
 //
 //  Component:    Signup
@@ -21,5 +22,29 @@ const Signup: NextPage = () => {
     </Center>
   );
 };
+
+//
+// Function:    getServerSideProps
+// Description: Fetches the initial data for this page
+// Parameters:  context: GetServerSidePropsContext - the context of the page
+// Returns:     default props
+//
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // If authentication works then we want to go to the index page
+  // Otherwise continue to signup
+  try {
+    await authenticate(context);
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+    };
+  } catch {
+    return {
+      props: {},
+    };
+  }
+}
 
 export default Signup;
