506a76430bd1dea207236c7ad97c32936f1c0603
Trent Thompson <tthompson@conestogac.on.ca>
2022-08-17 11:09:21 -0400

    add api url for production mode

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -5,6 +5,9 @@
 
 // URL for lastfm searching
 const lastFmURL = "http://ws.audioscrobbler.com/2.0/";
-const apiURL = "http://localhost:3000/api/v1";
+const apiURL =
+  process.env.NODE_ENV === "production"
+    ? "https://selectr.ca/api/v1"
+    : "http://localhost:3000/api/v1";
 
 export { lastFmURL, apiURL };
